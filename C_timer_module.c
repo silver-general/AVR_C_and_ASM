c255b99388faf3404604077ff585d513d742f50c
Morgi_93 <56215982+silver-general@users.noreply.github.com>
2020-03-13 17:20:16 +0100

    Update C_timer_module.c

--- C_timer_module.c
+++ C_timer_module.c
@@ -2,12 +2,12 @@
 LET'S INITIALIZE THE TIMER 0 AND COMMENT EVERYTHING WITH DATASHEET INFO
 */
 
-// SELECT WAVEFORM GENERATION (tab 11.5 for all modes)
+// SELECT FAST PWM (tab 11.5 for all modes) in TCCR0B,TCCR0A
 	//	WGM02 (TCCR0B),	WGM01 (TCCR0A),	WGM00 (TCCR0A),	MODE,	TOP,	update OCRx,	TOV flag set,
 	//	0				0				0				normal
 	// ...
-	// MODES: normal; PWM phase correct, CTC, fast PWM, reserved,PWM phase correct, reserved, fastPWM
-	// -> fast PWM with OCRx at TOP (so that OCR0A determines the duty cycle): 1,1,1 
+	// MODES: normal; PWM phase correct (TOP=0xFF), CTC, fast PWM(TOP=0xFF), reserved,PWM phase correct (TOP=OCR0A), reserved, fastPWM (TOP=OCR0A)
+	// -> fast PWM with OCRx as TOP (so that OCR0A determines the duty cycle): 1,1,1 
 	TCCR0B |= 1<<WGM02;
 	TCCR0A |= 1<<WGM01 | 1<<WGM00;
 	
@@ -16,7 +16,7 @@ LET'S INITIALIZE THE TIMER 0 AND COMMENT EVERYTHING WITH DATASHEET INFO
 	OCR0A = (duty/100)*255;	
 	
 	// SELECT TOGGLE MODE FOR OCR0A in TCCR0A (tab11.2) 
-	//	COM0A1/COM0B1	COM0A0/COM0B0	description
+	//	COM0A1/COM0B1	COM0A0/COM0B0	DESCRIPTION
 	//	0				0				disconnected	
 	//									toggle COM0xn on compare match
 	//									clear
@@ -24,21 +24,29 @@ LET'S INITIALIZE THE TIMER 0 AND COMMENT EVERYTHING WITH DATASHEET INFO
 	TCCR0A |= (1<<COM0A1); // "toggle"
 	
 	
-	// SELECT FAST PWM OUTPUT PIN 
+	// SELECT FAST PWM OUTPUT PIN in DDRB
 	// avaiable pins: see pinout. OC0A (or OC0B???) -----------------------------???
 	DDRB |= (1 << PB0);	// sets PB0 (pin 0 of PINB register) as output, that is also the pin for OC0A
 
 	
-	// INTERRUPTS
+	// INTERRUPTS 
 	
-	// activate the inturrupt. it will happen when timer counter 0 overflows
-	TIMSK |= 1<<TOIE0;
 	// set external interrupts (BEFORE THE PRESCALER. reason: see datasheet)
+	// this writes the first bit in the status register?? ---------------------------???
 	sei();
+	
+	// activate the inturrupt pins in TIMSK. 
+	// PINS		TOIE0: overflow
+	//			OCIE0A, OCIE0B : output compare flag for OCR0A or OCR0B
+	TIMSK |= 1<<TOIE0;
+	
+	// WHEN A COMPARE MATCH OCCURS, OCF0x flag is set in the timer counter 0 interrupt flag register
+	// where do I write this? in what section?
+	// i don't need this unless I am in assembly. in the future maybe
 	
 	
 	// PRESCALER SELECTION
-	
+	// reduces the system clk 
 	// frequency of fast PWM: f = clk / 256*N, with clk = 1MHz for attiny85 with default fuse
 	//see table 11.6
 	// 7 modes, ascending number. 5 for internal clok (see below), 2 for external	
@@ -52,19 +60,3 @@ LET'S INITIALIZE THE TIMER 0 AND COMMENT EVERYTHING WITH DATASHEET INFO
 		//			external clk on T0 pin. clock on falling edge
 		//			external clk on T0 pin. clock on rising edge.
 	TCCR0B &= ~(1<<CS00 |1<<CS01 | 1<<CS02); // 0,0,0 stops everything
-	
-	while (1)
-	{
-		duty += 10; if (duty>100) { duty=10; }
-		_delay_ms(100);
-	}
-
-	return 1;
-}
-
-
-// do something during the interrupt. does this work with attiny85?
-ISR(TIMER0_OVF_vect)
-{
-	OCR0A = (duty/100)*255;
-}
